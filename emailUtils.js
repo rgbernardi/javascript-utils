93be7a2a0a4cf67ec9d36c7bed2b402c05e9d52b
rgbernardi <44535771+rgbernardi@users.noreply.github.com>
2019-10-05 19:56:42 -0300

    Create emailUtils.js
    Add a new function to validate e-mail address

---
+++ emailUtils.js
@@ -0,0 +1,12 @@
+//////////////////////////////////////////////////////////////////
+//================================================================
+// Function to validate an E-mail.
+//================================================================
+// The parameter is regarding the e-mail address to be checked.
+// If none is informed <TRUE> is considered by default.
+//
+// Returns: Boolean.
+//
+// Author: Regis G Bernardi 
+// E-mail: rgbernardi @ gmail.com
+//////////////////////////////////////////////////////////////////
