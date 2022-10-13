44e240851f009f3cf9cf8308a39379827b004e5c
rgbernardi <44535771+rgbernardi@users.noreply.github.com>
2022-10-13 19:16:57 -0300

    Update emailUtils.js
    function name changed from isEmail for isValid

--- emailUtils.js
+++ emailUtils.js
@@ -10,7 +10,7 @@
 // Author: Regis G Bernardi 
 // E-mail: rgbernardi @ gmail.com
 //////////////////////////////////////////////////////////////////
-function isEmail(email){
+function isValid(email){
   var regex = /^(([À-ü^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return new Boolean(regex.test(String(email).toLowerCase()));
 }
